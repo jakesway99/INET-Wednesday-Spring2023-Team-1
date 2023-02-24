e42941e8a3c3f8594bb9813ddad591be637f6dcd
unknown <jacobswei@gmail.com>
2023-02-24 13:24:17 -0500

    added spotipy to requirements, changed name of profile edit

--- application/views.py
+++ application/views.py
@@ -6,10 +6,11 @@ from spotipy.oauth2 import SpotifyClientCredentials
 def home(request):
     return HttpResponse("Hello, world. You're at the NYUBeatBuddies application (on Julie's branch)")
 
+
 def profile_edit(request):
     client_credentials_manager = SpotifyClientCredentials()
 
-    tokenDict = client_credentials_manager.get_access_token()
-    token = tokenDict['access_token']
+    token_dict = client_credentials_manager.get_access_token()
+    token = token_dict['access_token']
     context = {'OAuth': token}
-    return render(request, 'application/profile_edit.html',context)
+    return render(request, 'application/profile_edit.html', context)
