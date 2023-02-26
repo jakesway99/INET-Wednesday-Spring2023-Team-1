eed6c09a9a108ab00e5f4ae243a006f668d91198
lillyengeler <lre250@nyu.edu>
2023-02-26 17:01:33 -0500

    reverting to old before pull

--- application/urls.py
+++ application/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    #path("", views.home, name="home"),
+    path("", views.view_profile, name="view_profile"),
     path("profile/edit", views.profile_edit, name = "profile_edit")
 ]
