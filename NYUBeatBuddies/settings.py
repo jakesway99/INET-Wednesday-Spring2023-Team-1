dc2ea6a0ab9d91c14074e1f6d41d882130c47043
Nikola Ganev <ganevniko@hotmail.com>
2023-03-10 18:20:32 -0500

    formatting

--- NYUBeatBuddies/settings.py
+++ NYUBeatBuddies/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 # STATIC_ROOT is for collectstatic
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = ('static',)
+STATICFILES_DIRS = ("static",)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
