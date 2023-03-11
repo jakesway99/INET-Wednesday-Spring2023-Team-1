1996ee7379d9bb7f7f1f41938053578e06a30612
Nikola Ganev <ganevniko@hotmail.com>
2023-03-10 20:18:54 -0500

    test

--- NYUBeatBuddies/settings.py
+++ NYUBeatBuddies/settings.py
@@ -101,7 +101,7 @@ if os.environ.get("DATABASE_NAME", None):  # is not None and "test" not in sys.a
         "USER": os.environ.get("DATABASE_USER", ""),
         "PASSWORD": os.environ.get("DATABASE_PASSWORD", ""),
         "HOST": os.environ.get("DATABASE_HOST", ""),
-        "PORT": 5432,
+        "PORT": "5432",
     }
 
 # Password validation
