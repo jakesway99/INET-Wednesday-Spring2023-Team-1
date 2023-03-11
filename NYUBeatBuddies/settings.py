54780fcfef2780bb8fd68660dfb85ac1aeb97819
Nikola Ganev <ganevniko@hotmail.com>
2023-03-10 20:09:34 -0500

    test

--- NYUBeatBuddies/settings.py
+++ NYUBeatBuddies/settings.py
@@ -94,15 +94,14 @@ DATABASES = {
     }
 }
 
-if os.environ.get("DATABASE_NAME", None) is not None and "test" not in sys.argv:
+if os.environ.get("DATABASE_NAME", None):  # is not None and "test" not in sys.argv:
     DATABASES["default"] = {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("DATABASE_NAME", ""),
         "USER": os.environ.get("DATABASE_USER", ""),
         "PASSWORD": os.environ.get("DATABASE_PASSWORD", ""),
         "HOST": os.environ.get("DATABASE_HOST", ""),
-        "PORT": "5432",
-        "TEST": {},
+        "PORT": 5432,
     }
 
 # Password validation
