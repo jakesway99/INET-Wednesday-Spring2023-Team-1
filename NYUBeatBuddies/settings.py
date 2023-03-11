854a2aab0e6ae6f307669c29165fd1c187e5507c
Nikola Ganev <ganevniko@hotmail.com>
2023-03-11 18:11:03 -0500

    flake8

--- NYUBeatBuddies/settings.py
+++ NYUBeatBuddies/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 from pathlib import Path
 from dotenv import load_dotenv
-import sys
+# import sys
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
