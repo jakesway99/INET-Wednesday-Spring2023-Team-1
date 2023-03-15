e4e04118f1024fe141160862c419acd16c424c6b
Julianna <juliannagoldbas@gmail.com>
2023-03-15 17:38:02 -0400

    cleaned up tests, should work

--- application/tests.py
+++ application/tests.py
@@ -17,8 +17,7 @@ class HomeTestCase(TestCase):
         )
         self.url = "/"
 
-    def test_profile_response_logged_in(self):
-        self.client.login(username=TEST_USER, password=TEST_USER_PASSWORD)
+    def test_profile_response_not_logged_in(self):
         response = self.client.get("/application/profile")
         self.assertEqual(response.status_code, 302)
 
@@ -27,14 +26,18 @@ class HomeTestCase(TestCase):
         # Test if the response status code is 301 - redirect to login
         self.assertEqual(response.status_code, 301)
 
-    # def test_account_register_page_exists(self):
-    #     response = self.client.get("/account/login/register")
-    #     self.assertEqual(response.status_code, 200)
+    def test_account_register_page_exists(self):
+        response = self.client.get("/account/register")
+        self.assertEqual(response.status_code, 200)
 
     def test_account_login_page_exists(self):
-        self.client.login(username=TEST_USER, password=TEST_USER_PASSWORD)
-        response = self.client.get("/application/profile")
-        self.assertEqual(response.status_code, 302)
+        response = self.client.get("/account/login")
+        self.assertEqual(response.status_code, 200)
+
+    # def test_account_login_page_exists(self):
+    #     self.client.login(username=TEST_USER, password=TEST_USER_PASSWORD)
+    #     response = self.client.get("/application/profile")
+    #     self.assertEqual(response.status_code, 302)
 
     def test_matches_section_exists(self):
         # response = self.client.get(self.url)
@@ -55,9 +58,10 @@ class HomeTestCase(TestCase):
         # self.assertEqual(heading.text, "A song that has meaning to you")
         return
 
-    # def test_discover_exists(self):
+    # def test_discover_people_exists(self):
     #     response = self.client.get('/application/profile')
     #     self.assertEqual(response.status_code, 200)
     #     soup = BeautifulSoup(response.content, "html.parser")
     #     button = soup.select_one('button')
     #     self.assertIsNotNone(button)
+    #     # self.assertEqual(button.text, "Discover People")
