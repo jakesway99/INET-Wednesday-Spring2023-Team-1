f5f8e5eb6783b4100ae026922ebc73198eadc90d
Nikola Ganev <ganevniko@hotmail.com>
2023-03-16 15:52:56 -0400

    functionality

--- application/forms.py
+++ application/forms.py
@@ -386,42 +386,57 @@ class AccountSettingsForm(UserChangeForm):
 
         self.helper.layout = Layout(
             Div(
+                Fieldset("First Name"),
                 Div(
                     Field(
                         "first_name",
-                        placeholder="",
+                        placeholder="John",
+                        label="dfsf",
                         css_class="form-group form-control-lg",
                     ),
                     css_class="col",
                 ),
+                Fieldset("Last Name"),
                 Div(
                     Field(
                         "last_name",
-                        placeholder="",
+                        placeholder="Doe",
                         css_class="form-group form-control-lg",
                     ),
                     css_class="col",
                 ),
+                Fieldset("Email"),
                 Div(
                     Field(
                         "email",
-                        placeholder="",
+                        placeholder="johndoe@email.com",
                         css_class="form-group form-control-lg",
                     ),
                     css_class="col",
                 ),
+                Fieldset("Current Password"),
                 Div(
                     Field(
                         "password",
-                        placeholder="",
+                        placeholder="Current Password",
                         css_class="form-group form-control-lg",
                     ),
                     css_class="col",
                 ),
+                Fieldset("New Password"),
                 Div(
                     Field(
                         "password",
-                        placeholder="",
+                        placeholder="New Password",
+                        css_class="form-group form-control-lg",
+                    ),
+                    css_class="col",
+                ),
+                Fieldset("Confirm New Password"),
+                Div(
+                    Field(
+                        "password",
+                        placeholder="Confirm New Password",
                         css_class="form-group form-control-lg",
                     ),
                     css_class="col",
@@ -429,7 +444,7 @@ class AccountSettingsForm(UserChangeForm):
                 css_class="form-col",
             ),
         )
-        self.helper.form_show_labels = True
+        self.helper.form_show_labels = False
 
     class Meta:
         model = User
