c90e65bd5d9d30b1f76880162898665949f1fafb
Jacob Wei <jacobswei@gmail.com>
2023-04-04 23:40:26 -0400

    adding more tests

--- application/tests.py
+++ application/tests.py
@@ -1,9 +1,16 @@
 from django.test import TestCase, Client, RequestFactory
 from django.contrib.auth.models import User
 from django.urls import reverse
-from .models import FavoriteGenre, FavoriteArtist, FavoriteSong, FavoriteAlbum, Account
+from .models import (
+    FavoriteGenre,
+    FavoriteArtist,
+    FavoriteSong,
+    FavoriteAlbum,
+    Account,
+    UserPrompts,
+)
 import os
-from .views import discover_events
+from .views import discover_events, profile_edit, profile
 
 # from bs4 import BeautifulSoup
 
@@ -72,97 +79,179 @@ class HomeTestCase(TestCase):
 class Profile(TestCase):
     def setUp(self):
         self.client = Client()
-        self.url = reverse("application:profile_edit")
-        self.user = User.objects.create_user(
-            username="testuser", password="@12345678", email="testing123@example.com"
+        self.request_factory = RequestFactory()
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user1 = User.objects.create_user(username="TEST_USER", password="@1234567")
+        cls.Acct = Account.objects.create(
+            user=cls.user1,
+            first_name="John",
+            last_name="Doe",
+            birth_year=1996,
+            location="NYC",
+            profile_picture="placeholder",
         )
-        self.account = Account.objects.create(
-            user=self.user,
-            first_name="testFirst",
-            last_name="testLast",
-            birth_year=1995,
+        cls.fav_song = FavoriteSong.objects.create(
+            user=cls.user1,
+            song1_name_artist="Kill Bill - SZA",
+            song2_name_artist="Sure Thing - Miguel",
+            song3_name_artist="Whenever, Wherever - Shakira",
+            song4_name_artist="Wasted On You - Morgan Wallen",
+            song5_name_artist="And We Knew It Was Our Time - Lane 8, Massane",
+            song1_id="3OHfY25tqY28d16oZczHc8",
+            song2_id="0JXXNGljqupsJaZsgSbMZV",
+            song3_id="2lnzGkdtDj5mtlcOW2yRtG",
+            song4_id="3cBsEDNhFI9E82vPj3kvi3",
+            song5_id="2tIv3DITNBTTayAM6Rewqi",
+        )
+
+        cls.fav_album = FavoriteAlbum.objects.create(
+            user=cls.user1,
+            album1_name_artist="Endless Summer Vacation - Miley Cyrus",
+            album2_name_artist="evermore - Taylor Swift",
+            album3_name_artist="Purple (Rainbow Friends) - Rockit Music",
+            album4_name_artist="RENAISSANCE - Beyoncé",
+            album5_name_artist="Bluey the Album - Bluey",
+            album1_id="0HiZ8fNXwJOQcrf5iflrdz",
+            album2_id="5jmVg7rwRcgd6ARPAeYNSm",
+            album3_id="0uw5PIXZiA3Kp8B6qBqPIo",
+            album4_id="6FJxoadUE4JNVwWHghBwnb",
+            album5_id="4ke6cauk7sHuydZCrkgD7s",
+        )
+
+        cls.fav_artist = FavoriteArtist.objects.create(
+            user=cls.user1,
+            artist1_name="Harry Styles",
+            artist2_name="Taylor Swift",
+            artist3_name="The 1975",
+            artist4_name="The Weeknd",
+            artist5_name="BTS",
+            artist1_id="6KImCVD70vtIoJWnq6nGn3",
+            artist2_id="6KImCVD70vtIoJWnq6nGn3",
+            artist3_id="3mIj9lX2MWuHmhNCA7LSCW",
+            artist4_id="1Xyo4u8uXC1ZmMpatF05PJ",
+            artist5_id="3Nrfpe0tUJi4K4DXYWgMUX",
+        )
+
+        cls.fav_genre = FavoriteGenre.objects.create(
+            user=cls.user1,
+            genre1="pop",
+            genre2="vaporwave",
+            genre3="melodic dubstep",
+            genre4="pop rock",
+            genre5="kpop",
+        )
+
+        cls.user_prompts = UserPrompts.objects.create(
+            user=cls.user1,
+            prompt1="A song that has meaning to you ",
+            prompt2="A song that everyone should listen to",
+            prompt3="A song that makes you want to dance",
+            prompt4="A song that makes you sad",
+            prompt5="A song that makes you think about life",
+            response1="Die For You - Remix - The Weeknd, Ariana Grande",
+            response2="El Gordo Trae El Mando - Chino Pacas",
+            response3="Fast Car - Tracy Chapman",
+            response4="Glimpse of Us - Joji",
+            response5="Romantic Homicide - d4vd",
         )
 
     def test_add_profile_edit(self):
-        data = {"username": "testuser", "password": "@12345678"}
-        response = self.client.post(reverse("account:login"), data, follow=True)
+        self.client.force_login(self.user1)
 
         data = {
-            "song1_name_artist": "Kill Bill - SZA",
-            "song2_name_artist": "Sure Thing - Miguel",
-            "song3_name_artist": "Whenever, Wherever - Shakira",
-            "song4_name_artist": "Wasted On You - Morgan Wallen",
-            "song5_name_artist": "And We Knew It Was Our Time - Lane 8, Massane",
-            "song1_id": "3OHfY25tqY28d16oZczHc8",
-            "song2_id": "0JXXNGljqupsJaZsgSbMZV",
-            "song3_id": "2lnzGkdtDj5mtlcOW2yRtG",
-            "song4_id": "3cBsEDNhFI9E82vPj3kvi3",
-            "song5_id": "2tIv3DITNBTTayAM6Rewqi",
-            "album1_name_artist": "Endless Summer Vacation - Miley Cyrus",
-            "album2_name_artist": "evermore - Taylor Swift",
-            "album3_name_artist": "Purple (Rainbow Friends) - Rockit Music",
-            "album4_name_artist": "RENAISSANCE - Beyoncé",
-            "album5_name_artist": "Bluey the Album - Bluey",
-            "album1_id": "0HiZ8fNXwJOQcrf5iflrdz",
-            "album2_id": "5jmVg7rwRcgd6ARPAeYNSm",
-            "album3_id": "0uw5PIXZiA3Kp8B6qBqPIo",
-            "album4_id": "6FJxoadUE4JNVwWHghBwnb",
-            "album5_id": "4ke6cauk7sHuydZCrkgD7s",
-            "artist1_name": "Harry Styles",
-            "artist2_name": "Taylor Swift",
-            "artist3_name": "The 1975",
-            "artist4_name": "The Weeknd",
-            "artist5_name": "BTS",
-            "artist1_id": "6KImCVD70vtIoJWnq6nGn3",
-            "artist2_id": "6KImCVD70vtIoJWnq6nGn3",
-            "artist3_id": "3mIj9lX2MWuHmhNCA7LSCW",
-            "artist4_id": "1Xyo4u8uXC1ZmMpatF05PJ",
-            "artist5_id": "3Nrfpe0tUJi4K4DXYWgMUX",
-            "genre1": "pop",
-            "genre2": "vaporwave",
-            "genre3": "melodic dubstep",
-            "genre4": "pop rock",
-            "genre5": "kpop",
-            "response5": "Nothing Else Matters (Remastered) - Metallica",
-            "first_name": "John",
-            "last_name": "Doe",
+            "song1_name_artist": "song1_test",
+            "song2_name_artist": "song2_test",
+            "song3_name_artist": "song3_test",
+            "song4_name_artist": "song4_test",
+            "song5_name_artist": "song5_test",
+            "song1_id": "song1_test",
+            "song2_id": "song2_test",
+            "song3_id": "song3_test",
+            "song4_id": "song4_test",
+            "song5_id": "song5_test",
+            "album1_name_artist": "album1_test",
+            "album2_name_artist": "album2_test",
+            "album3_name_artist": "album3_test",
+            "album4_name_artist": "album4_test",
+            "album5_name_artist": "album5_test",
+            "album1_id": "album1_test",
+            "album2_id": "album2_test",
+            "album3_id": "album3_test",
+            "album4_id": "album4_test",
+            "album5_id": "album5_test",
+            "artist1_name": "artist1_test",
+            "artist2_name": "artist2_test",
+            "artist3_name": "artist3_test",
+            "artist4_name": "artist4_test",
+            "artist5_name": "artist5_test",
+            "artist1_id": "artist1_test",
+            "artist2_id": "artist2_test",
+            "artist3_id": "artist3_test",
+            "artist4_id": "artist4_test",
+            "artist5_id": "artist5_test",
+            "genre1": "genre1_test",
+            "genre2": "genre2_test",
+            "genre3": "genre3_test",
+            "genre4": "genre4_test",
+            "genre5": "genre5_test",
+            "prompt1": "A song that has meaning to you ",
+            "prompt2": "A song that everyone should listen to",
+            "prompt3": "A song that makes you want to dance",
+            "prompt4": "A song that makes you sad",
+            "prompt5": "A song that makes you think about life",
+            "response1": "response1_test",
+            "response2": "response2_test",
+            "response3": "response3_test",
+            "response4": "response4_test",
+            "response5": "response5_test",
+            "first_name": "testfirst",
+            "last_name": "testlast",
             "birth_year": "1990",
-            "location": "Bushwick",
+            "location": "testlocation",
         }
-        response = self.client.post(self.url, data, follows=True)
+        request = self.request_factory.post(reverse("application:profile_edit"), data)
+        request.user = self.user1
+        response = profile_edit(request)
         self.assertEqual(response.status_code, 302)
+        fav_song = FavoriteSong.objects.get(user=self.user1)
+        self.assertEqual(fav_song.song1_name_artist, "song1_test")
+        self.assertEqual(fav_song.song3_name_artist, "song3_test")
+        self.assertEqual(fav_song.song5_name_artist, "song5_test")
+        self.assertEqual(fav_song.song1_id, "song1_test")
+        self.assertEqual(fav_song.song3_id, "song3_test")
+        self.assertEqual(fav_song.song5_id, "song5_test")
 
-        fav_song = FavoriteSong.objects.get(user=self.user)
-        self.assertEqual(fav_song.song1_name_artist, "Kill Bill - SZA")
-        self.assertEqual(fav_song.song3_name_artist, "Whenever, Wherever - Shakira")
-        self.assertEqual(
-            fav_song.song5_name_artist, "And We Knew It Was Our Time - Lane 8, Massane"
-        )
-        self.assertEqual(fav_song.song1_id, "3OHfY25tqY28d16oZczHc8")
-        self.assertEqual(fav_song.song3_id, "2lnzGkdtDj5mtlcOW2yRtG")
-        self.assertEqual(fav_song.song5_id, "2tIv3DITNBTTayAM6Rewqi")
+        fav_album = FavoriteAlbum.objects.get(user=self.user1)
+        self.assertEqual(fav_album.album1_name_artist, "album1_test")
+        self.assertEqual(fav_album.album3_name_artist, "album3_test")
+        self.assertEqual(fav_album.album5_name_artist, "album5_test")
+        self.assertEqual(fav_album.album1_id, "album1_test")
+        self.assertEqual(fav_album.album5_id, "album5_test")
 
-        fav_album = FavoriteAlbum.objects.get(user=self.user)
-        self.assertEqual(
-            fav_album.album1_name_artist, "Endless Summer Vacation - Miley Cyrus"
-        )
-        self.assertEqual(
-            fav_album.album3_name_artist, "Purple (Rainbow Friends) - Rockit Music"
-        )
-        self.assertEqual(fav_album.album5_name_artist, "Bluey the Album - Bluey")
-        self.assertEqual(fav_album.album1_id, "0HiZ8fNXwJOQcrf5iflrdz")
-        self.assertEqual(fav_album.album5_id, "4ke6cauk7sHuydZCrkgD7s")
+        fav_artist = FavoriteArtist.objects.get(user=self.user1)
+        self.assertEqual(fav_artist.artist1_name, "artist1_test")
+        self.assertEqual(fav_artist.artist5_name, "artist5_test")
+        self.assertEqual(fav_artist.artist1_id, "artist1_test")
+        self.assertEqual(fav_artist.artist5_id, "artist5_test")
 
-        fav_artist = FavoriteArtist.objects.get(user=self.user)
-        self.assertEqual(fav_artist.artist1_name, "Harry Styles")
-        self.assertEqual(fav_artist.artist5_name, "BTS")
-        self.assertEqual(fav_artist.artist1_id, "6KImCVD70vtIoJWnq6nGn3")
-        self.assertEqual(fav_artist.artist5_id, "3Nrfpe0tUJi4K4DXYWgMUX")
+        fav_genre = FavoriteGenre.objects.get(user=self.user1)
+        self.assertEqual(fav_genre.genre1, "genre1_test")
+        self.assertEqual(fav_genre.genre2, "genre2_test")
+        self.assertEqual(fav_genre.genre5, "genre5_test")
 
-        fav_genre = FavoriteGenre.objects.get(user=self.user)
-        self.assertEqual(fav_genre.genre1, "pop")
-        self.assertEqual(fav_genre.genre2, "vaporwave")
-        self.assertEqual(fav_genre.genre5, "kpop")
+        prompts = UserPrompts.objects.get(user=self.user1)
+        self.assertEqual(prompts.response1, "response1_test")
+        self.assertEqual(prompts.response3, "response3_test")
+        self.assertEqual(prompts.response5, "response5_test")
+
+    def testProfileView(self):
+        self.client.force_login(self.user1)
+        request = self.request_factory.get(reverse("application:profile"))
+        request.user = self.user1
+        response = profile(request)
+        self.assertEqual(response.status_code, 200)
 
 
 class DiscoverEvents(TestCase):
