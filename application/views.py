e373f3e84bcb77cda6ffb5c31b9a002aa78d2795
lillyengeler <lre250@nyu.edu>
2023-04-12 00:49:19 -0400

    flake8

--- application/views.py
+++ application/views.py
@@ -423,10 +423,10 @@ def profile(request):
     context.update({"going_to_events": going_to_events})
 
     # remove old events from interested/going list
-    tz = timezone('EST')
+    tz = timezone("EST")
     curr_date_time = datetime.datetime.now(tz)
-    curr_date_pre = curr_date_time.strftime('%Y-%m-%d')
-    curr_date = datetime.datetime.strptime(str(curr_date_pre), '%Y-%m-%d').date()
+    curr_date_pre = curr_date_time.strftime("%Y-%m-%d")
+    curr_date = datetime.datetime.strptime(str(curr_date_pre), "%Y-%m-%d").date()
 
     interested_events, going_to_events = getSavedEvents(curr_user)
 
@@ -436,14 +436,14 @@ def profile(request):
         saved_events_object = SavedEvents.objects.create(user=request.user)
 
     saved_events_object.interestedEvents = (
-            []
-            if saved_events_object.interestedEvents is None
-            else saved_events_object.interestedEvents
-        )
+        []
+        if saved_events_object.interestedEvents is None
+        else saved_events_object.interestedEvents
+    )
     saved_events_object.goingToEvents = (
-            []
-            if saved_events_object.goingToEvents is None
-            else saved_events_object.goingToEvents
+        []
+        if saved_events_object.goingToEvents is None
+        else saved_events_object.goingToEvents
     )
 
     for item in interested_events:
@@ -454,7 +454,7 @@ def profile(request):
                 # remove the event from the table
                 saved_events_object.interestedEvents.remove(int(curr_event))
                 saved_events_object.save()
-            
+
     for item in going_to_events:
         curr_event = item[-2]
         # remove going to event if the event has already passed
@@ -463,8 +463,6 @@ def profile(request):
                 # remove the event from the table
                 saved_events_object.goingToEvents.remove(int(curr_event))
                 saved_events_object.save()
-
-
 
     if request.method == "POST":
         curr_event = request.POST.get("item")
@@ -680,11 +678,6 @@ def getDiscoverProfile(request):
 def discover_events(request):
     event_list = []
     all_events = EventList.objects.all()
-    tz = timezone('EST')
-    curr_date_time = datetime.datetime.now(tz)
-    curr_date_pre = curr_date_time.strftime('%Y-%m-%d')
-    curr_date = datetime.datetime.strptime(str(curr_date_pre), '%Y-%m-%d').date()
-
     for event in all_events:
         time_string = event.start_time
         if event.start_date < datetime.date.today():
@@ -700,7 +693,9 @@ def discover_events(request):
             event_time_final = std_time
 
         # needed to remove old events from interested/going lists
-        this_event_date = datetime.datetime.strptime(str(event.start_date), '%Y-%m-%d').date()
+        this_event_date = datetime.datetime.strptime(
+            str(event.start_date), "%Y-%m-%d"
+        ).date()
 
         # getting month name and day number from datetime obj
         month_num = event.start_date.month
@@ -731,16 +726,14 @@ def discover_events(request):
     interested_events_pk = []
     going_to_events_pk = []
 
-
     for item in interested_events:
         curr_event = item[-2]
         interested_events_pk.append(curr_event)
-            
+
     for item in going_to_events:
         curr_event = item[-2]
         going_to_events_pk.append(curr_event)
 
-    
     context = {}
     context.update({"profile_picture": account.profile_picture})
     context.update({"first_name": account.first_name})
@@ -916,10 +909,6 @@ def getSavedEvents(user):
 
 def getEventList(user_events):
     saved_events = []
-    tz = timezone('EST')
-    curr_date_time = datetime.datetime.now(tz)
-    curr_date_pre = curr_date_time.strftime('%Y-%m-%d')
-    curr_date = datetime.datetime.strptime(str(curr_date_pre), '%Y-%m-%d').date()
     for event_id in user_events:
         event = EventList.objects.get(pk=event_id)
         if event.start_date < datetime.date.today():
@@ -934,7 +923,9 @@ def getEventList(user_events):
             std_time = mil_time.strftime("%-I:%M" "%p").lower()
             # std_time = mil_time.strftime("%M").lower()
             event_time_final = std_time
-            this_event_date = datetime.datetime.strptime(str(event.start_date), '%Y-%m-%d').date()
+            this_event_date = datetime.datetime.strptime(
+                str(event.start_date), "%Y-%m-%d"
+            ).date()
         saved_events.append(
             (
                 event.event_name,
